6c8f21dc42152a77cb39e0ce45b8c09f901f8311
Marc Brillault <marc@vestiairecollective.com>
2018-05-03 19:31:25 +0200

    The old PHP way

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/init.php';
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
-$request  = Request::createFromGlobals();
 $name     = $request->get('name', 'World');
 $name     = htmlspecialchars($name, ENT_QUOTES, 'UTF-8');
-$response = new Response(sprintf('Hello, %s !', $name));
+$response = $response->setContent(sprintf('Hello, %s !', $name));
 $response->send();
