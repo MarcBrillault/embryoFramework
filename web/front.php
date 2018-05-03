f94913f8fd4a5318979b6ed7a54d748b0923c3be
Marc Brillault <marc@vestiairecollective.com>
2018-05-03 20:38:38 +0200

    Routing

--- web/front.php
+++ web/front.php
@@ -4,34 +4,30 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
 
-$request  = Request::createFromGlobals();
-$response = new Response();
-
+$request       = Request::createFromGlobals();
+$path          = $request->getPathInfo();
+$response      = new Response();
+$routes        = include_once __DIR__ . '/../src/app.php';
 $templatesPath = __DIR__ . '/../src/pages/%s.php';
-$map           = [
-    '/hello' => 'hello',
-    '/bye'   => 'bye',
-];
+$loaded        = false;
+$context       = new RequestContext();
+$context->fromRequest($request);
+$matcher = new UrlMatcher($routes, $context);
 
-$path = $request->getPathInfo();
-
-$loaded = false;
-
-if (isset($map[$path])) {
-    $filePath = sprintf($templatesPath, $map[$path]);
-    if (file_exists($filePath)) {
-        $loaded = true;
-        ob_start();
-        extract($request->query->all(), EXTR_SKIP);
-        include $filePath;
-        $response->setContent(ob_get_clean());
-    }
-}
-
-if (!$loaded) {
+try {
+    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
+    ob_start();
+    include(sprintf($templatesPath, $_route));
+    $response->setContent(ob_get_clean());
+} catch (\Symfony\Component\Routing\Exception\ResourceNotFoundException $e) {
     $response->setStatusCode(404);
-    $response->setContent('Not found');
+    $response->setContent('Not Found');
+} catch (Exception $e) {
+    $response->setStatusCode(500);
+    $response->setContent($e->getMessage());
 }
 
 $response->send();
