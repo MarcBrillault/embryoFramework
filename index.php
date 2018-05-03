7424ce79e6d70994803c9f864e3d2778094f959b
Marc Brillault <marc@vestiairecollective.com>
2018-05-03 19:20:21 +0200

    HttpFoundation

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
-header('Content-Type: text/html; charset=utf-8');
 
-$name = $_GET['name'] ?: 'World';
+require_once __DIR__ . '/vendor/autoload.php';
 
-printf('Hello, %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8'));
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request  = Request::createFromGlobals();
+$name     = $request->get('name', 'World');
+$name     = htmlspecialchars($name, ENT_QUOTES, 'UTF-8');
+$response = new Response(sprintf('Hello, %s !', $name));
+$response->send();
